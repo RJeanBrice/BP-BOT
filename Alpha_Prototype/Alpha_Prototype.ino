4f56ed3a42bbb9b695bfbd0284db5138672360af
RJeanBrice <reginald.jeanbrice@makerbot.com>
2016-10-20 15:57:02 -0400

    updating alpha code

--- Alpha_Prototype/Alpha_Prototype.ino
+++ Alpha_Prototype/Alpha_Prototype.ino
@@ -11,31 +11,140 @@
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-LiquidCrystal lcd(12, 11, 10, 8, 9, 7);
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *LeftMotor = AFMS.getMotor(1);
 // You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *RightMotor = AFMS.getMotor(2);
 
 
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
-    lcd.begin(16, 2);
+ lcd.begin(16, 2);
 
 
-      lcd.clear();
-      lcd.print(" Auto Jog Mode! ");
-      lcd.setCursor(0,1); 
-      lcd.print("     READY!");
+  lcd.clear();
+  lcd.print(" BEER PONG BOT! ");
+  lcd.setCursor(0,1); 
+  lcd.print("     READY!");
 
-      AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  AFMS.begin();  // create with the default frequency 1.6KHz
+//AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+
+  forward(150,159);
+  delay(1000);
+  left(150,150);
+  delay(1000);
+  backward(150,150);
+  delay(1000);
+  right(150,150);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+ int count = 0;
+ 
+ while (count < 6) {
+  
+  forward(150,159);
+  delay(250);
+  left(150,150);
+  delay(250);
+  backward(150,150);
+  delay(250);
+  right(150,150);
+  delay(250);
+
+  count++;
+ }
+
+ 
+}
+
+void forward(int SetSpeed, double cycles) {
+   uint8_t i;
+
+  Serial.println("moving forward");
+
+  LeftMotor->run(FORWARD);
+  RightMotor->run(FORWARD);
+  
+  for (i=0; i<cycles; i++) {
+    LeftMotor->setSpeed(SetSpeed);  
+    RightMotor->setSpeed(SetSpeed);  
+    delay(10);
+    Serial.println(i);
+  }
+
+  Serial.println("shut motor off");
+  
+  LeftMotor->run(RELEASE);
+  RightMotor->run(RELEASE);  
+
+  return;
+}
+
+void backward(int SetSpeed, int cycles) {
+   uint8_t i;
+
+  Serial.print("moving forward");
+
+  LeftMotor->run(BACKWARD);
+  RightMotor->run(BACKWARD);
+  
+  for (i=0; i<cycles; i++) {
+    LeftMotor->setSpeed(SetSpeed);  
+    RightMotor->setSpeed(SetSpeed);  
+    delay(10);
+  }
+  
+  LeftMotor->run(RELEASE);
+  RightMotor->run(RELEASE);  
+  
+
+  return;
+}
+
+void left(int SetSpeed, int cycles) {
+   uint8_t i;
+
+  Serial.print("moving forward");
+
+  LeftMotor->run(BACKWARD);
+  RightMotor->run(FORWARD);
+  
+  for (i=0; i<cycles; i++) {
+    LeftMotor->setSpeed(SetSpeed);  
+    RightMotor->setSpeed(SetSpeed);  
+    delay(10);
+  }
+  LeftMotor->run(RELEASE);
+  RightMotor->run(RELEASE);  
+  
+
+  return;
+}
+
+void right(int SetSpeed, int cycles) {
+   uint8_t i;
+
+  Serial.print("moving forward");
+
+  LeftMotor->run(FORWARD);
+  RightMotor->run(BACKWARD);
+  
+  for (i=0; i<cycles; i++) {
+    LeftMotor->setSpeed(SetSpeed);  
+    RightMotor->setSpeed(SetSpeed);  
+    delay(10);
+  }
+  LeftMotor->run(RELEASE);
+  RightMotor->run(RELEASE);  
+  
+
+  return;
 }
